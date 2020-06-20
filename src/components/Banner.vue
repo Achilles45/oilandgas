b56c1c842c4b87d08002b1e93ea219b1f79cc64f
Achilles45 <achillesusuoyibo7@gmail.com>
2020-06-19 22:33:27 -0700

    Changed banner text

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -18,7 +18,7 @@
             <h1 class="animated slideInDown animate3d">YOUR INVESTMENT IS <span class="orange">SAFE & SECURED</span></h1>
             <p
               class="animated slideInRight animate3d"
-            >We leverage bank grade security to encrypt all our investors data to make sure their investments is 100% safe and secured</p>
+            >Initial investment is compulsory. 100% of what ever you donated in the system</p>
              <router-link to="/signup" class="carousel-btn animated zoomIn animate3d">Get Started <i class="fas fa-long-arrow-alt-right"></i> </router-link>
           </div>
         </div>
